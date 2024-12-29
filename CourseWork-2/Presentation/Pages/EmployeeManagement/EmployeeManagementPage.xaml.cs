b4f33da76cbe03cd725973582c5daf32b81cf34b
sedooj <sergo54ru@gmail.com>
2024-12-30 04:20:45 +0700

    Added the table for employees of the company. There is a "View" button in the line, this button will navigate to the employee page where we can fire him 😄(or something else)

--- CourseWork-2/Presentation/Pages/EmployeeManagement/EmployeeManagementPage.xaml.cs
+++ CourseWork-2/Presentation/Pages/EmployeeManagement/EmployeeManagementPage.xaml.cs
@@ -45,20 +45,14 @@ namespace CourseWork_2.Presentation.Pages.EmployeeManagement
             {
                 bool isEmployee = _controller.IsEmployee(_selectedCompany, _selectedHuman);
                 InviteButton.IsVisible = !isEmployee;
-                DismissButton.IsVisible = isEmployee;
+                PositionEntry.IsVisible = !isEmployee;
             }
         }
 
         private void OnInviteClicked(object sender, EventArgs e)
         {
-            _controller.InviteEmployee(_selectedCompany, _selectedHuman, "Position");
-            UpdateButtonsVisibility();
-            LoadEmployees();
-        }
-
-        private void OnDismissClicked(object sender, EventArgs e)
-        {
-            _controller.DismissEmployee(_selectedCompany, _selectedHuman);
+            string position = PositionEntry.Text;
+            _controller.InviteEmployee(_selectedCompany, _selectedHuman, position);
             UpdateButtonsVisibility();
             LoadEmployees();
         }
@@ -71,10 +65,22 @@ namespace CourseWork_2.Presentation.Pages.EmployeeManagement
                 {
                     Number = index + 1,
                     Name = e.UserDefaultCredentials.FirstName,
-                    Position = e.Position
+                    Position = e.Position,
+                    UUID = e.UUID
                 }).ToList();
 
                 EmployeesCollectionView.ItemsSource = employees;
+            }
+        }
+
+        private void OnViewClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var employeeUUID = button?.CommandParameter as string;
+            var employee = _selectedCompany?.Employees.FirstOrDefault(e => e.UUID == employeeUUID);
+            if (employee != null)
+            {
+                // Handle view button click logic here
             }
         }
     }
