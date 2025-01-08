82a6dea10cffd4eedf1dbc952f2e6e003bba0b35
sedooj <sergo54ru@gmail.com>
2025-01-08 23:11:24 +0700

    EmployeeManagementPage and EmployeeManagementPageController has been refactored

--- CourseWork-2/Data/Controllers/EmployeeManagementPageController.cs
+++ CourseWork-2/Data/Controllers/EmployeeManagementPageController.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics;
 using CourseWork_2.Data.Service;
 using CourseWork_2.Domain.Models;
 using CourseWork_2.Domain.Service;
+using CourseWork_2.Presentation.Pages.EmployeeManagement;
+using CourseWork_2.Presentation.Util;
 
 namespace CourseWork_2.Data.Controllers
 {
@@ -51,27 +54,34 @@ namespace CourseWork_2.Data.Controllers
             Humans = _humanStorageService.LoadEntities(humanDirectoryPath).ToList();
         }
 
+
         public bool IsEmployee(Company company, Guid humanUuid)
         {
             return company.EmployeeUUIDs.Contains(humanUuid);
         }
 
-        public void InviteEmployee(string position)
+        public void InviteEmployee(Entry position, Button inviteButton, Entry positionEntry,
+            CollectionView employeesCollectionView, Picker humanPicker)
         {
-            if (SelectedCompany != null && SelectedHuman != null && !IsEmployee(SelectedCompany, SelectedHuman.Uuid))
-            {
-                _hrDepartmentService.InviteEmployee(SelectedCompany, SelectedHuman.Uuid, position);
-                LoadData();
-            }
+            if (SelectedCompany == null || SelectedHuman == null ||
+                IsEmployee(SelectedCompany, SelectedHuman.Uuid)) return;
+            _hrDepartmentService.InviteEmployee(SelectedCompany, SelectedHuman.Uuid, position.Text);
+            SelectedHuman = null;
+            LoadData();
+            UpdateInviteButtonVisibility(inviteButton, positionEntry);
+            LoadEmployees(employeesCollectionView);
+            humanPicker.SelectedIndex = -1;
         }
 
         public void PromoteEmployee(Guid employeeUuid, string newPosition, string reason)
         {
             var promotion = _companyService.PromoteEmployee(employeeUuid, newPosition, reason);
-            if (promotion) GiveReward(SelectedHuman!, new Reward(id: Guid.NewGuid().ToString(), Reward.RewardType.Promotion, DateTime.Now, reason));
+            if (promotion)
+                GiveReward(SelectedHuman!,
+                    new Reward(id: Guid.NewGuid().ToString(), Reward.RewardType.Promotion, DateTime.Now, reason));
             LoadData();
         }
-        
+
         public void DemoteEmployee(Guid employeeUuid, string newPosition, string reason)
         {
             _companyService.DemoteEmployee(employeeUuid, newPosition, reason);
@@ -149,6 +159,103 @@ namespace CourseWork_2.Data.Controllers
             {
                 Console.WriteLine($"Error in PunishEmployee: {ex.Message}");
             }
+        }
+
+        private void LoadHumans(Picker humansPicker)
+        {
+            humansPicker.ItemsSource = Humans
+                ?.Select(h => h.UserDefaultCredentials.FirstName + " " + h.UserDefaultCredentials.LastName).ToList();
+        }
+
+        private void LoadCompanies(Picker companyPicker)
+        {
+            var companyNames = Companies?.Select(c => c.Name).ToList();
+            companyPicker.ItemsSource = companyNames;
+        }
+
+        public void UpdatePickers(Picker companyPicker, Picker humansPicker)
+        {
+            LoadCompanies(companyPicker);
+            LoadHumans(humansPicker);
+        }
+
+        public void LoadEmployees(CollectionView employeesCollectionView)
+        {
+            if (SelectedCompany != null)
+            {
+                var employees = SelectedCompany.EmployeeUUIDs.Select((id, index) =>
+                {
+                    var employee = _humanStorageService.LoadEntity($"{Config.HumanStoragePath}{id.ToString()}");
+                    return new
+                    {
+                        Number = index + 1,
+                        Name = employee?.UserDefaultCredentials.FirstName + " " +
+                               employee?.UserDefaultCredentials.LastName,
+                        Position = employee?.LastEmploymentHistoryRecord?.PositionAtWork,
+                        Id = id
+                    };
+                }).ToList();
+                employeesCollectionView.ItemsSource = employees;
+            }
+        }
+
+        public void UpdateInviteButtonVisibility(Button inviteButton, Entry positionEntry)
+        {
+            bool isVisible = SelectedHuman != null &&
+                             SelectedCompany != null &&
+                             !IsEmployee(SelectedCompany, SelectedHuman.Uuid);
+
+            inviteButton.IsVisible = isVisible;
+            positionEntry.IsVisible = isVisible;
+        }
+
+        public void OnCompanySelect(Picker companyPicker, Picker humanPicker, Frame tableFrame,
+            Label companyEmployeesLabel, Button inviteButton, Entry positionEntry,
+            CollectionView employeesCollectionView)
+        {
+            SelectedCompany = Companies?[companyPicker.SelectedIndex];
+            bool isCompanySelected = SelectedCompany != null;
+            tableFrame.IsVisible = isCompanySelected;
+            companyEmployeesLabel.IsVisible = isCompanySelected;
+            UpdateInviteButtonVisibility(inviteButton, positionEntry);
+            UpdatePickers(companyPicker, humanPicker);
+            LoadEmployees(employeesCollectionView);
+        }
+
+        public void OnHumanSelect(Button inviteButton, Entry positionEntry, Picker humanPicker)
+        {
+            if (humanPicker.SelectedIndex >= 0 && humanPicker.SelectedIndex < Humans?.Count)
+            {
+                SelectedHuman = Humans?[humanPicker.SelectedIndex];
+                UpdateInviteButtonVisibility(inviteButton, positionEntry);
+            }
+        }
+
+        public void OnAppearing(Picker companyPicker, Picker humanPicker, Button inviteButton, Entry positionEntry,
+            CollectionView employeesCollectionView)
+        {
+            LoadData();
+            UpdatePickers(companyPicker, humanPicker);
+            LoadEmployees(employeesCollectionView);
+            UpdateInviteButtonVisibility(inviteButton, positionEntry);
+        }
+
+        public bool OnViewClicked(object sender)
+        {
+            var button = sender as Button;
+            if (button?.CommandParameter is Guid employeeUuid)
+            {
+                SelectedHuman = _humanStorageService.LoadEntity($"{Config.HumanStoragePath}{employeeUuid.ToString()}");
+
+                if (SelectedHuman == null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- CourseWork-2/Presentation/Pages/EmployeeManagement/EmployeeManagementPage.xaml.cs
+++ CourseWork-2/Presentation/Pages/EmployeeManagement/EmployeeManagementPage.xaml.cs
@@ -1,15 +1,10 @@
-using CourseWork_2.Domain.Models;
-using System.Diagnostics;
 using CourseWork_2.Data.Controllers;
-using CourseWork_2.Data.Service;
-using CourseWork_2.Presentation.Util;
 
 namespace CourseWork_2.Presentation.Pages.EmployeeManagement
 {
     public partial class EmployeeManagementPage
     {
         private readonly EmployeeManagementPageController _controller = new();
-        private readonly LocalStorageService<Human> _humanStorageService = new();
 
         public EmployeeManagementPage()
         {
@@ -19,190 +14,34 @@ namespace CourseWork_2.Presentation.Pages.EmployeeManagement
 
         private void InitPage()
         {
-            try
-            {
-                _controller.LoadData();
-                LoadData();
-                Debug.WriteLine("EmployeeManagementPage initialized successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error initializing EmployeeManagementPage: {ex}");
-            }
+            _controller.LoadData();
+            _controller.UpdatePickers(CompanyPicker, HumanPicker);
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            try
-            {
-                _controller.LoadData();
-                LoadData();
-                LoadEmployees();
-                UpdateButtonsVisibility();
-                Debug.WriteLine("OnAppearing executed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in OnAppearing: {ex}");
-            }
+            _controller.OnAppearing(CompanyPicker, HumanPicker, InviteButton, PositionEntry, EmployeesCollectionView);
         }
 
         private void OnInviteClicked(object sender, EventArgs e)
         {
-            Debug.WriteLine("OnInviteClicked started.");
-            try
-            {
-                string position = PositionEntry.Text;
-                _controller.InviteEmployee(position);
-                _controller.SelectedHuman = null;
-                _controller.LoadData();
-                LoadData();
-                UpdateButtonsVisibility();
-                LoadEmployees();
-                HumanPicker.SelectedIndex = -1;
-                Debug.WriteLine("OnInviteClicked executed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in OnInviteClicked: {ex}");
-            }
-        }
-        
-        private void LoadData()
-        {
-            try
-            {
-                var companyNames = _controller.Companies?.Select(c => c.Name).ToList();
-                CompanyPicker.ItemsSource = companyNames;
-                Debug.WriteLine($"Loaded companies: {string.Join(", ", companyNames ?? new List<string>())}");
-
-                HumanPicker.ItemsSource = _controller.Humans?.Select(h => h.UserDefaultCredentials.FirstName).ToList();
-                
-                Debug.WriteLine("LoadData executed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in LoadData: {ex}");
-            }
+            _controller.InviteEmployee(PositionEntry, InviteButton, PositionEntry, EmployeesCollectionView, HumanPicker);
         }
 
         private void OnCompanySelected(object sender, EventArgs e)
         {
-            try
-            {
-                _controller.SelectedCompany = _controller.Companies?[CompanyPicker.SelectedIndex];
-                bool isCompanySelected = _controller.SelectedCompany != null;
-                TableFrame.IsVisible = isCompanySelected;
-                CompanyEmployeesLabel.IsVisible = isCompanySelected;
-                LoadData();
-                LoadEmployees();
-                Debug.WriteLine("OnCompanySelected executed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in OnCompanySelected: {ex}");
-            }
+            _controller.OnCompanySelect(CompanyPicker, HumanPicker, TableFrame, CompanyEmployeesLabel, InviteButton, PositionEntry, EmployeesCollectionView);
         }
 
         private void OnHumanSelected(object sender, EventArgs e)
         {
-            try
-            {
-                if (HumanPicker.SelectedIndex >= 0 && HumanPicker.SelectedIndex < _controller.Humans?.Count)
-                {
-                    _controller.SelectedHuman = _controller.Humans?[HumanPicker.SelectedIndex];
-                    UpdateButtonsVisibility();
-                    Debug.WriteLine("OnHumanSelected executed successfully.");
-                }
-                else
-                {
-                    Debug.WriteLine("Invalid HumanPicker.SelectedIndex.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in OnHumanSelected: {ex}");
-            }
-        }
-
-        private void UpdateButtonsVisibility()
-        {
-            Debug.WriteLine("UpdateButtonsVisibility started.");
-            try
-            {
-                bool isEmployee = _controller.SelectedHuman != null &&
-                                  _controller.SelectedCompany != null &&
-                                  !_controller.IsEmployee(_controller.SelectedCompany, _controller.SelectedHuman.Uuid);
-
-                InviteButton.IsVisible = isEmployee;
-                PositionEntry.IsVisible = isEmployee;
-
-                Debug.WriteLine("UpdateButtonsVisibility executed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in UpdateButtonsVisibility: {ex}");
-            }
-        }
-
-        private void LoadEmployees()
-        {
-            Debug.WriteLine("LoadEmployees started.");
-            try
-            {
-                if (_controller.SelectedCompany != null)
-                {
-                    var employees = _controller.SelectedCompany.EmployeeUUIDs.Select((id, index) =>
-                    {
-                        var employee = _humanStorageService.LoadEntity($"{Config.HumanStoragePath}{id.ToString()}");
-                        return new
-                        {
-                            Number = index + 1,
-                            Name = employee?.UserDefaultCredentials.FirstName + " " +
-                                   employee?.UserDefaultCredentials.LastName,
-                            Position = employee?.LastEmploymentHistoryRecord?.PositionAtWork,
-                            Id = id
-                        };
-                    }).ToList();
-                    EmployeesCollectionView.ItemsSource = employees;
-                }
-                Debug.WriteLine("LoadEmployees executed successfully.");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in LoadEmployees: {ex}");
-            }
+            _controller.OnHumanSelect(InviteButton, PositionEntry, HumanPicker);
         }
         
         private async void OnViewClicked(object sender, EventArgs e)
         {
-            try
-            {
-                Debug.WriteLine("OnViewClicked started.");
-                var button = sender as Button;
-                if (button?.CommandParameter is Guid employeeUuid)
-                {
-                    _controller.SelectedHuman = _humanStorageService.LoadEntity($"{Config.HumanStoragePath}{employeeUuid.ToString()}");
-
-                    if (_controller.SelectedHuman == null)
-                    {
-                        Debug.WriteLine("Cant OnViewClicked, Loaded human is null.");
-                        return;
-                    }
-
-                    await Navigation.PushAsync(new EmployeePage(_controller));
-                    Debug.WriteLine("OnViewClicked executed successfully.");
-                }
-                else
-                {
-                    Debug.WriteLine("Cant OnViewClicked, Employee UUID is null or invalid.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error in OnViewClicked: {ex}");
-            }
+            if (_controller.OnViewClicked(sender)) await Navigation.PushAsync(new EmployeePage(_controller));
         }
     }
 }
